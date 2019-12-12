8177be8035f17d6d4f1de5efdd2e113dd9a611ad
Porot <lauroleslauroles@gmail.com>
2019-12-12 15:25:43 +0100

    Update app.component.ts

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SocketService } from './services/socket.service';
+import { NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
