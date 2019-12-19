877cd2d888d8a920337f17f0751787f34e783ecf
PC-JULIE\Julie <julie.palmaro@gmail.com>
2019-12-19 18:04:38 +0100

    lost + win services + update + delete batch

--- applicatif/src/app.controller.ts
+++ applicatif/src/app.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AppGateway } from './app.gateway';
 
 @Controller()
 export class AppController {
   // constructor(private readonly appService: AppService) { }
+  constructor(private readonly appGateway: AppGateway) { }
 
   @Get()
   getInfos(): void {
     // this.appService.getValidSlug();
     // this.appService.getBatch();
+    // this.appGateway.deleteMessage('uv efdpooft qfqf, uftu');
+    // this.appGateway.getFirstBatchUndone();
   }
 }
--- applicatif/src/app.gateway.ts
+++ applicatif/src/app.gateway.ts
@@ -6,7 +6,7 @@ import { AppController } from './app.controller';
 export class AppGateway {
   @WebSocketServer() server: Server;
   // Ensemble des messages à décrypter
-  messages = ['whvw, wx ghfrqqhv shsh', 'fg pqoazzqe bqbq, fqef'];
+  messages = ['uv efdpooft qfqf, uftu', 'whvw, wx ghfrqqhv shsh', 'fg pqoazzqe bqbq, fqef'];
 
   // Ensemble des clés possibles
   maxKey = 50;
@@ -16,10 +16,12 @@ export class AppGateway {
   // Les différents états sont : undone, in progress, found, not found
   batchs = [];
 
+  clientId = '';
+
   constructor() {
     // Création de tous les batchs pour tous les messages
     for (let i = 0; i < this.messages.length; i++) {
-      for (let j = 0; j < this.maxKey; j += 10) {
+      for (let j = 1; j < this.maxKey; j += 10) {
           this.batchs.push([i, j, j + 10, 'undone']);
         }
       }
@@ -31,45 +33,114 @@ export class AppGateway {
   handleMessage(client: Socket, data: string) {
     console.log(data);
   }
+
   @SubscribeMessage('lost')
-  lostMessage(client: Socket, data: string) {
-    console.log("perdu", data);
+  lostMessage(client: Socket, data: any) {
+    console.log('perdu', data);
+    this.updateStateOfBatch(data.message, data.begin, data.end, 'not found');
+
+    this.clientId = client.id;
+    this.getValidSlug();
+    this.getBatch();
+    this.getAlgo();
+  }
+
+  @SubscribeMessage('win')
+  winMessage(client: Socket, data: any) {
+    // if ('tu deconnes pepe' in data.messageDecrypt) {
+      console.log('gagné', data);
+      this.updateStateOfBatch(data.message, data.begin, data.end, 'found');
+
+      // Supprimer le messages de la liste et des batchs
+      this.deleteMessage(data.message);
+
+      // Envoi de l'arrêt à tous les clients
+      this.sendEventToClient('stop', { message: data.message });
+
+      this.clientId = client.id;
+      this.getValidSlug();
+      this.getBatch();
+      this.getAlgo();
+    // }
   }
 
   // communication serveur -> client
   public sendEventToClient(str: string, data: any) {
+    this.server.to(this.clientId).emit(str, data);
+  }
+
+  public sendEventToClients(str: string, data: any) {
     this.server.clients().emit(str, data);
+    // envoi à un seul client
+    //this.server.to(this.clientId).emit(str, data);
   }
 
   // Connexion d'un client
   handleConnection(client: Socket, ...args: []) {
     console.log('client connecté ' + client.id);
+    this.clientId = client.id;
     this.getValidSlug();
-    this.getAlgo();
     this.getBatch();
+    this.getAlgo();
   }
 
-
-  // SERVICES
+  // FONCTIONS
    // Récupération du premier batch disponible (qui a l'état undone)
   getFirstBatchUndone(): number {
     for (let i = 0; i < this.batchs.length; i++) {
-        console.log('search ' + this.batchs[i]);
         if (this.batchs[i][3] === 'undone') {
           return i;
         }
       }
     return null;
   }
-  
+
+  // Modification de l'état d'un batch
+  updateStateOfBatch(message, begin, end, state): void {
+    const index = this.messages.indexOf(message);
+    let find = false;
+    let i = 0;
+    while (!find && i < this.batchs.length) {
+      if (this.batchs[i][0] === index && this.batchs[i][1] === begin && this.batchs[i][2] === end) {
+        this.batchs[i][3] = state;
+        find = true;
+      }
+      i += i;
+    }
+  }
+
+  // Suppression d'un message
+  deleteMessage(message): void {
+    const index = this.messages.indexOf(message);
+    let i = 0;
+    while (i < this.batchs.length) {
+      if (this.batchs[i][0] === index) {
+        delete(this.batchs[i]);
+      }
+      i++;
+    }
+
+    // Suppression des éléments vides du tableau
+    this.batchs = this.batchs.filter((el) => {
+      if (el != null) {
+        return el;
+      }
+    });
+    this.messages.slice(index, 1);
+  }
+
+
+  // SERVICES
   // Envoi du slug
   getValidSlug(): void {
+    // console.log('slug client connecté ' + this.clientId);
     this.sendEventToClient('slug', { slug: 'Tu deconnes pepe' });
   }
 
   // Envoi l'algo pour décoder le message
   // L'algo retourne la clé de début si le message est décodé et -1 sinon
   getAlgo(): void {
+    console.log('algo client connecté ' + this.clientId);
     const algorithme = (message, begin, end, slug) => {
       const regex = new RegExp(slug, 'i');
       for (let i = begin; i <= end; i++) {
@@ -78,31 +149,32 @@ export class AppGateway {
         messageTab.forEach((car: string) => {
           const ascii = car.charCodeAt(0);
           if (ascii >= 'A'.charCodeAt(0) && ascii <= 'Z'.charCodeAt(0)) {
-            const calcul = (ascii - begin - 'A'.charCodeAt(0)) % 26;
+            const calcul = (ascii - i - 'A'.charCodeAt(0)) % 26;
             car = String.fromCharCode(calcul + 'A'.charCodeAt(0));
           } else if (ascii >= 'a'.charCodeAt(0) && ascii <= 'z'.charCodeAt(0)) {
-            const calcul = (ascii - begin - 'a'.charCodeAt(0)) % 26;
+            const calcul = (ascii - i - 'a'.charCodeAt(0)) % 26;
             car = String.fromCharCode(calcul + 'a'.charCodeAt(0));
           }
           newMessage += car;
         });
+        console.log('new message', newMessage);
         if (newMessage.search(regex) !== -1) {
           return [i, newMessage];
         }
       }
-      return null;
+      return -1;
     };
-    this.sendEventToClient('algo', { algo: algorithme });
+    this.sendEventToClient('algo', { algo: algorithme.toString() });
   }
 
   // Envoi du message chiffré + clé de début + clé de fin
   getBatch(): void {
+    // console.log('batch client connecté ' + this.clientId);
     const ind = this.getFirstBatchUndone();
     if (ind != null) {
       const keys = this.batchs[ind];
       this.sendEventToClient('batch', { message: this.messages[keys[0]], begin: keys[1], end: keys[2]});
       this.batchs[ind][3] = 'in progress';
-      console.log(this.batchs);
     } else {
       //
     }
